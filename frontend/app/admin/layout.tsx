28f1a53369171740be4dc0cd42271bf0b1e56018
dobby <hsjoo@aitrics.com>
2025-01-12 20:32:55 +0900

    add : admin dashboard ui

--- frontend/app/admin/layout.tsx
+++ frontend/app/admin/layout.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { SideBar } from '@/components/admin/layouts/SideBar';
 import { NavBar } from '@/components/admin/layouts/NavBar';
 
-export default function layout({children}) {
+export default function layout({children} : { children: React.ReactNode }) {
   return (
     <div>
         <SideBar />
